26fbbbe10ef4346cd9e018d440080a63157a18b0
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-13 22:18:25 +0100

    Update layout

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -15,23 +15,26 @@ export default function SearchBar({ plants, onSearch }: SearchBarProps) {
 	return (
 		<SearchManager plants={plants} onSearch={onSearch}>
 			{({ searchTerm, handleSearch, handleClear }) => (
-				<View className="flex-row items-center justify-center border border-gray-300 rounded-lg bg-slate m-2">
-					<View className="absolute left-2.5">
-						<Icon
-							name="magnifyingGlass"
-							size={30}
-							colour="text-gray-500"
+				<View className="flex-row items-center justify-center rounded-2xl bg-slate-200">
+					<View className="flex-row items-center justify-center rounded-xl bg-slate-200 px-2.5">
+						<View className="flex-row items-center flex-1">
+							<Icon
+								name="magnifyingGlass"
+								size={30}
+								colour="text-gray-600"
+							/>
+						</View>
+						<TextInput
+							value={searchTerm}
+							onChangeText={handleSearch}
+							placeholder="Search"
+							className="flex-1 text-base py-3 text-center placeholder-gray-600"
+							placeholderTextColor="#4B5563"
 						/>
 					</View>
-					<TextInput
-						value={searchTerm}
-						onChangeText={handleSearch}
-						placeholder="Search"
-						className="flex-1 text-base py-3 text-center"
-					/>
 					{searchTerm.length > 0 && (
 						<TouchableOpacity onPress={handleClear} className="p-2.5">
-							<Icon name="close" size={30} colour="text-gray-500" />
+							<Icon name="close" size={30} colour="text-gray-600" />
 						</TouchableOpacity>
 					)}
 				</View>
