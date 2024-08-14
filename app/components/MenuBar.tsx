0e32e407e6c16910e2958b92033fa5568a68d58c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-14 10:06:38 +0100

    Add animation for controls buttons

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -1,51 +1,52 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-native-fontawesome';
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons/faEllipsis';
-import tw from 'twrnc';
+import { View, Text, TouchableOpacity, Animated } from 'react-native';
+
+import Icon from './Icon';
 
 interface MenuBarProps {
 	plantCount: number;
+	controlsVisible: boolean;
+	onToggleControls: () => void;
 }
 
-export default function MenuBar({ plantCount }: MenuBarProps) {
+export default function MenuBar({
+	plantCount,
+	controlsVisible,
+	onToggleControls,
+}: MenuBarProps) {
+	const rotateAnim = React.useRef(
+		new Animated.Value(controlsVisible ? 1 : 0)
+	).current;
+
+	React.useEffect(() => {
+		Animated.timing(rotateAnim, {
+			toValue: controlsVisible ? 1 : 0,
+			duration: 300,
+			useNativeDriver: true,
+		}).start();
+	}, [controlsVisible, rotateAnim]);
+
+	const rotate = rotateAnim.interpolate({
+		inputRange: [0, 1],
+		outputRange: ['0deg', '-180deg'],
+	});
+
 	return (
-		<View style={styles.header}>
-			<View style={styles.counterContainer}>
-				<Text style={styles.counter}>{plantCount}</Text>
+		<View className="flex-row justify-center mt-5">
+			<View className="absolute left-4">
+				<Icon name="leaf" colour="text-green-600" size={40} />
 			</View>
-			<TouchableOpacity style={styles.menuButton}>
-				<Text>
-					<Icon
-						icon={faEllipsis}
-						size={40}
-						color={tw.color('text-slate-800')}
-					/>
-				</Text>
+			<View className="flex-1 items-center">
+				<Text className="text-4xl font-bold">{plantCount}</Text>
+			</View>
+			<TouchableOpacity
+				className="absolute right-4"
+				onPress={onToggleControls}
+			>
+				<Animated.View style={{ transform: [{ rotate }] }}>
+					<Icon name="chevron" colour="text-green-600" size={40} />
+				</Animated.View>
 			</TouchableOpacity>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	header: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center',
-		borderBottomWidth: 1,
-		borderBottomColor: '#e0e0e0',
-		paddingBottom: 20,
-	},
-	counterContainer: {
-		flex: 1,
-		alignItems: 'center',
-	},
-	counter: {
-		fontSize: 40,
-		fontWeight: 'bold',
-	},
-	menuButton: {
-		position: 'absolute',
-		right: 15,
-	},
-});
