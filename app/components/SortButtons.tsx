cdf8df812d64ee16d2b33cd9bdeb5ec8ffbc7cab
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-14 10:04:27 +0100

    Style buttons with Nativewind

--- app/components/SortButtons.tsx
+++ app/components/SortButtons.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+
 import { SortMethod } from '../types';
 
 interface SortButtonsProps {
@@ -13,17 +13,17 @@ export default function SortButtons({
 }: SortButtonsProps) {
 	const renderButton = (method: SortMethod, label: string) => (
 		<TouchableOpacity
-			style={[
-				styles.button,
-				method === currentSortMethod && styles.activeButton,
-			]}
+			className={`
+        bg-slate-200 p-3 rounded-lg m-1
+        ${method === currentSortMethod ? 'bg-green-300' : ''}
+      `}
 			onPress={() => onSortMethodChange(method)}
 		>
 			<Text
-				style={[
-					styles.text,
-					method === currentSortMethod && styles.activeText,
-				]}
+				className={`
+          text-sm font-semibold
+          ${method === currentSortMethod ? 'text-black' : 'text-gray-600'}
+        `}
 			>
 				{label}
 			</Text>
@@ -31,37 +31,9 @@ export default function SortButtons({
 	);
 
 	return (
-		<View style={styles.sortButtons}>
+		<>
 			{renderButton('alphabetical', 'Alphabetical')}
 			{renderButton('category', 'Category')}
-		</View>
+		</>
 	);
 }
-
-const styles = StyleSheet.create({
-	sortButtons: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		marginVertical: 10,
-	},
-	button: {
-		backgroundColor: 'white',
-		padding: 12,
-		borderRadius: 6,
-		borderWidth: 1,
-		borderColor: '#D1D5DB', // gray-300
-	},
-	activeButton: {
-		backgroundColor: '#16a34a', // green-600
-		borderColor: '#16a34a', // green-600
-	},
-	text: {
-		fontSize: 14,
-		fontWeight: '600',
-		textTransform: 'uppercase',
-		color: '#111827', // gray-900
-	},
-	activeText: {
-		color: 'white',
-	},
-});
