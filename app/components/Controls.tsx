9d42441de653a9cc1f50b506adb1862f2ec05484
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-14 10:06:52 +0100

    Style controls with Nativewind

--- app/components/Controls.tsx
+++ app/components/Controls.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 
 import { Plant, SortMethod } from '../types';
 
+import SearchBar from './SearchBar';
+import ResetButton from './ResetButton';
 import SortButtons from './SortButtons';
-import Search from './Search';
 
 interface ControlsProps {
 	onSortMethodChange: (method: SortMethod) => void;
 	currentSortMethod: SortMethod;
 	onSearch: (searchResults: Plant[]) => void;
 	plants: Plant[];
+	onReset: () => void;
 }
 
 export default function Controls({
@@ -18,20 +19,24 @@ export default function Controls({
 	currentSortMethod,
 	onSearch,
 	plants,
+	onReset,
 }: ControlsProps) {
 	return (
-		<View style={styles.container}>
-			<Search onSearch={onSearch} plants={plants} />
-			<SortButtons
-				onSortMethodChange={onSortMethodChange}
-				currentSortMethod={currentSortMethod}
-			/>
+		<View className="mt-3 mx-3">
+			<View className="mb-3">
+				<SearchBar onSearch={onSearch} plants={plants} />
+			</View>
+			<View className="flex-row justify-between ">
+				<View className="max-w-1/3">
+					<ResetButton onReset={onReset} />
+				</View>
+				<View className="flex-2 flex-row justify-around border border-gray-300 rounded-lg">
+					<SortButtons
+						onSortMethodChange={onSortMethodChange}
+						currentSortMethod={currentSortMethod}
+					/>
+				</View>
+			</View>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		marginBottom: 10,
-	},
-});
