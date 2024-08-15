1564f3e268f5b28c3305fd4d889ef505865f1ece
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-15 10:40:11 +0100

    Style modal

--- app/components/LogoButton.tsx
+++ app/components/LogoButton.tsx
@@ -32,33 +32,39 @@ export default function LogoButton() {
 				<BlurView intensity={10} tint="dark" style={{ flex: 1 }}>
 					<View className="flex-1 justify-center items-center px-5">
 						<View className="bg-white rounded-3xl p-6 items-center shadow-lg w-full max-w-md">
-							<Text className="text-xl font-bold text-green-700">
-								Plant Counter
-							</Text>
-
-							<View className="w-20 h-6">
-								<Image
-									source={require('../../assets/images/appStore.png')}
-									className="h-full w-full"
-									resizeMode="contain"
-								/>
-							</View>
-
-							<View className="w-32 h-32 mb-6">
-								<Image
-									source={require('../../assets/images/icon.png')}
-									className="w-full h-full rounded-lg"
-								/>
-							</View>
-
-							<Text className="mb-6 text-center text-2xl font-bold">
+							<Text className="mb-12 text-center text-3xl font-bold">
 								I've eaten{' '}
-								<Text className="bg-green-600 py-1 px-2 rounded text-white">
-									{' '}
-									{checkedPlants.size}{' '}
-								</Text>{' '}
+								<View className="bg-green-600 rounded-md p3">
+									<Text className="text-white text-3xl">
+										{' '}
+										{checkedPlants.size}{' '}
+									</Text>
+								</View>{' '}
 								plant{useSingular ? '' : 's'} so far this week
 							</Text>
+
+							<View className="flex-row h-28 w-full justify-between mb-12">
+								<View className="flex-col justify-end">
+									<View className="w-20 h-6">
+										<Image
+											source={require('../../assets/images/appStore.png')}
+											className="h-full w-full"
+											resizeMode="contain"
+										/>
+									</View>
+
+									<Text className="text-base font-bold text-green-700">
+										Plant Counter
+									</Text>
+								</View>
+
+								<View className="w-28 h-28 mb-6">
+									<Image
+										source={require('../../assets/images/icon.png')}
+										className="w-full h-full rounded-lg"
+									/>
+								</View>
+							</View>
 
 							<Pressable
 								className="bg-green-600 py-3 px-6 rounded-lg"
