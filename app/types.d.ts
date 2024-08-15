875c0e4f87b6aa855d352c2c33831291f1994012
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-15 11:21:32 +0100

    Add Category type

--- app/types.d.ts
+++ app/types.d.ts
@@ -3,9 +3,34 @@ export interface Plant {
 	category: Category;
 }
 
-export type SortMethod = 'alphabetical' | 'category';
+export type Category =
+	| 'Citrus fruits'
+	| 'Berries'
+	| 'Stone fruits'
+	| 'Tropical fruits'
+	| 'Pome fruits'
+	| 'Melons'
+	| 'Fruits as vegetables'
+	| 'Other vegetables'
+	| 'Leafy vegetables'
+	| 'Cruciferous vegetables'
+	| 'Root vegetables'
+	| 'Squash & gourds'
+	| 'Alliums'
+	| 'Nuts'
+	| 'Seeds'
+	| 'Grains & pseudo-grains'
+	| 'Legumes'
+	| 'Brassicas'
+	| 'Herbs'
+	| 'Spices'
+	| 'Fungi'
+	| 'Stems & stalks'
+	| 'Seaweed & algae'
+	| 'Flowers';
 
 export interface PlantCategory {
 	title: string;
 	data: Plant[];
 }
+export type SortMethod = 'alphabetical' | 'category';
